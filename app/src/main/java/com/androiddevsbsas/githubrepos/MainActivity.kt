4daeca14b0306a82ea99dfe9d733ab49dac96181
Ezequiel Zanetta <ezequielzanetta@gmail.com>
2017-11-18 10:52:56 -0300

    Add exercise steps

--- app/src/main/java/com/androiddevsbsas/githubrepos/MainActivity.kt
+++ app/src/main/java/com/androiddevsbsas/githubrepos/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* TODO EJERCICIO
+        * Consumir el listado de repositorios publicos utilizando la clase GitHubApiClient
+        * GitHubApiClient.gitHubApi.getPublicRepositories() // Devuelve un listado de GitHubRepo. List<GitHubRepo>
+        * Crear un adapter para mostrarlos los repos en un RecyclerView. Utilizar layout item_repo.xml
+        * Configurar e inicializar RecyclerView y adapter
+        * */
+
     }
 }
